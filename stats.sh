39b0ac62318da343f03e40ead4ae34b644acc920
Sriram Sami <frizensami@gmail.com>
2016-09-04 17:00:04 +0800

    Modified stats to only show cycles and instructions:

--- stats.sh
+++ stats.sh
@@ -1,8 +1,11 @@
 MATRIXSIZE=1000
 #echo "Performance for MMSEQ (Sequential)"
 #perf stat -e cache-references -e cache-misses -e cycles -e instructions -- ./mmseq $MATRIXSIZE
-echo "Performance for MM1 (Parallel)"
+echo "======== Performance for MM1 (Parallel) - INSTRUCTIONS ============="
 for NUMTHREADS in 1 2 4 8 16 32 32 16 8 4 2 1
 do
-  perf stat --log-fd 1 -e cache-references -e cache-misses -e cycles -e instructions -- ./mm1 $MATRIXSIZE $NUMTHREADS
+  perf stat --log-fd 1 -e cycles -e instructions -- ./mm1 $MATRIXSIZE $NUMTHREADS
+  
 done
+
+
